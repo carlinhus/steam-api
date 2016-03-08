49d03932462b956463098dc9d481a1d480c30bf0
carlinhus <carlinhus@carlinhus.com>
2016-03-08 06:54:44 +0100

    Added view ingame option

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 import urllib2
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import json
 
 """
@@ -10,19 +10,28 @@ sent to the function. steamID is a get param on the url.
 """
 def showCSGOInventory(request, steamID):
     #Prepare api request
-    apiRequest = urllib2.Request("http://steamcommunity.com/id/" + steamID + "/inventory/json/730/2/es", headers={"Accept-language" : "es"})
+    apiRequest = urllib2.Request("http://steamcommunity.com/id/" + steamID + "/inventory/json/730/2", headers={"Accept-language" : "es"})
     #Executing api request and saving it as String (JSON format comes)
     jsondata = urllib2.urlopen(apiRequest).read()
     #Parsing json to Python Dict
     jsonDecoded = json.loads(jsondata)
 
     #Get and appends background color for item name
-    
+    options = list()
     for k, v in jsonDecoded["rgDescriptions"].iteritems():
         for k1 in v["tags"]:
             if k1.has_key("color"):
                 v["background"] = k1["color"]
+        if(v.has_key("market_actions")):
+            v["actions"] = v.get("market_actions")[0]
+        else:
+            v["actions"] = False
+        
+ 
+    # DEV RETURN
+    # return JsonResponse(options, safe=False)
 
-    #Returns items description to render engine
+
+    # Returns items description to render engine
     return render_to_response("inventory.html", {"items": jsonDecoded["rgDescriptions"]}, context_instance=RequestContext(request))
 
